2b178df2122620530ab67a2a0bb06e7984cce355
kenriortega <kenriortega@gmail.com>
2021-06-30 16:18:53 -0400

    feat: add flag to define  port pass as parameter

--- services/micro-a/api.go
+++ services/micro-a/api.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,10 +11,13 @@ import (
 )
 
 func main() {
+	var port int
+	flag.IntVar(&port, "port", 8081, "Port to serve")
+	flag.Parse()
 	r := mux.NewRouter()
 	r.HandleFunc("/api/v1/health/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "Status: %v\n", "up")
+		fmt.Fprintf(w, "Status: %v\n", port)
 	})
 	r.HandleFunc("/api/v1/version/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
@@ -22,7 +26,7 @@ func main() {
 
 	srv := &http.Server{
 		Handler: r,
-		Addr:    "127.0.0.1:8000",
+		Addr:    fmt.Sprintf(":%d", port),
 		// Good practice: enforce timeouts for servers you create!
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
