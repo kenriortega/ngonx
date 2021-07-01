fb3b365f83c7b216f1f1479e8b45b630ed486d03
kenriortega <kenriortega@gmail.com>
2021-07-01 15:29:26 -0500

    feat: add custom errors and logger with zap

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/gorilla/mux v1.8.0
-	github.com/spf13/viper v1.8.1 // indirect
+	github.com/spf13/viper v1.8.1
+	go.uber.org/zap v1.17.0
 )
---
+++ internal/logger.go
@@ -0,0 +1,36 @@
+package internal
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+var log *zap.Logger
+
+func init() {
+	var err error
+	config := zap.NewProductionConfig()
+	encoderConfig := zap.NewProductionEncoderConfig()
+	encoderConfig.TimeKey = "timestamp"
+	encoderConfig.StacktraceKey = ""
+	encoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.EncoderConfig = encoderConfig
+
+	log, err = config.Build(zap.AddCallerSkip(1))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func LogInfo(message string, fields ...zap.Field) {
+	log.Info(message, fields...)
+}
+func LogDebug(message string, fields ...zap.Field) {
+	log.Debug(message, fields...)
+}
+func LogError(message string, fields ...zap.Field) {
+	log.Error(message, fields...)
+}
+func LogWarn(message string, fields ...zap.Field) {
+	log.Warn(message, fields...)
+}
