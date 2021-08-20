fb203ac033cac8fe9b556b1c3bae0bffb73245a0
kenriortega <kenriortega@gmail.com>
2021-08-20 09:51:37 -0400

    reafactor: change metrics registrations and prometheus keys collectors

--- pkg/metric/client.go
+++ pkg/metric/client.go
@@ -11,16 +11,6 @@ import (
 )
 
 var (
-	CountersByRoute = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "counter_by_routes",
-			Help: "Register all access by routes using counter metrics",
-		},
-		[]string{
-			"service",
-			"path",
-		},
-	)
 	CountersByEndpoint = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "counter_request_by_microservicio",
@@ -35,7 +25,7 @@ var (
 	)
 
 	DurationHttpRequest = promauto.NewHistogramVec(prometheus.HistogramOpts{
-		Name: "http_response_time_seconds",
+		Name: "http_response_time_seconds_microservices",
 		Help: "Duration of HTTP requests.",
 	}, []string{"path"})
 
@@ -44,24 +34,14 @@ var (
 			Name: "http_requests_total",
 			Help: "Number of get requests.",
 		},
-		[]string{"path"},
-	)
-
-	ResponseStatus = prometheus.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "response_status",
-			Help: "Status of HTTP response",
-		},
-		[]string{"status"},
+		[]string{"path", "service"},
 	)
 )
 
 func init() {
-	prometheus.MustRegister(CountersByRoute)
+	prometheus.Register(DurationHttpRequest)
 	prometheus.MustRegister(CountersByEndpoint)
-	prometheus.MustRegister(DurationHttpRequest)
 	prometheus.MustRegister(TotalRequests)
-	prometheus.MustRegister(ResponseStatus)
 }
 
 func ExposeMetricServer(configPort int) {
