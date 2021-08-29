376e61c138f5ff3f4e819092bb69420b9b7f86da
kenriortega <kenriortega@gmail.com>
2021-08-29 13:06:58 -0400

    refactor: Add method for parsing map to struct

--- internal/mngt/domain/endpoint.go
+++ internal/mngt/domain/endpoint.go
@@ -1,11 +1,17 @@
 package mngt
 
+import (
+	"encoding/json"
+
+	"github.com/kenriortega/ngonx/pkg/logger"
+)
+
 type Endpoint struct {
 	pathUrl string
 	status  string
 }
 type IEndpoint interface {
-	ListEnpoints() []Endpoint
+	ListEnpoints() ([]Endpoint, error)
 	RegisterEnpoint(Endpoint) error
 }
 
@@ -20,3 +26,12 @@ func (ed *Endpoint) PathUrl() string           { return ed.pathUrl }
 func (ed *Endpoint) SetPathUrl(pathUrl string) { ed.pathUrl = pathUrl }
 func (ed *Endpoint) Status() string            { return ed.status }
 func (ed *Endpoint) SetStatus(status string)   { ed.status = status }
+
+func (ed *Endpoint) FromMapToJSON(data map[string]interface{}) {
+	b, err := json.Marshal(data)
+	if err != nil {
+		logger.LogError(err.Error())
+	}
+
+	json.Unmarshal(b, &ed)
+}
