bd659b2a2fc499a0ca39c19570965ae280260fc4
kenriortega <kenriortega@gmail.com>
2021-11-01 13:52:32 -0400

    feat: start beta ngonx-ui

---
+++ web/ngonx-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Endpoints from './components/Endpoints'
+function App() {
+  return (
+    <>
+      <Header />
+      <main className={"main"}>
+        <Endpoints />
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default App
