f6d87fed44a88218abbfb5da09865d17f6e13b15
kenriortega <kenriortega@gmail.com>
2021-11-08 10:58:43 -0500

    ref+fix: change logic for webSocket hook

--- web/src/hooks/useSocket.js
+++ web/src/hooks/useSocket.js
@@ -1,53 +1,67 @@
 import { useState, useEffect, useMemo, useCallback } from "react";
-
+import { useToast } from "@chakra-ui/react"
 export const useSocket = (url) => {
+	const toast = useToast()
 	const [data, setData] = useState(null);
 	const [error, setError] = useState(null);
 	const socket = useMemo(
-		() => new WebSocket(`ws://localhost:10001/${url}`),
+		() => new WebSocket(`ws://0.0.0.0:10001/${url}`),
 		[]
 	);
 
+	socket.onopen = useCallback(
+		(event) => {
+			toast({
+				title: "Conected",
+				description: `ws://0.0.0.0:10001/${url}`,
+				status: "success",
+				duration: 5000,
+				position: "top-right",
+				isClosable: true,
+			})
+			socket.send("endpoints")
+		},
+		[socket]
+	)
+
 	socket.onmessage = useCallback(
 		(event) => {
-			console.log("Socket message: ", data);
-			return setData(event.data);
+			if (event.data !== null) {
+				return setData(JSON.parse(event.data));
+			}
 		},
 		[socket]
 	);
 
 	socket.onerror = useCallback(
-		(error) => {
-			console.log("Socket error: ", error);
-			setError(error);
+		(event) => {
+			toast({
+				title: "Error",
+				description: "error",
+				status: "error",
+				duration: 5000,
+				position: "top-right",
+				variant: "subtle",
+				isClosable: true,
+			})
+			setError(event);
 		},
 		[socket]
 	);
 
 	socket.onclose = useCallback(
 		(event) => {
-			if (event.wasClean) {
-				setError(
-					`Connection closed cleanly, code=${event.code} reason=${event.reason}`
-				);
-			} else {
-				//  server process killed or network down
-				setError("Connection died");
-			}
+			toast({
+				title: "Closed",
+				description: `Closed Socket`,
+				status: "error",
+				duration: 5000,
+				position: "top-right",
+				isClosable: true,
+			})
 		},
 		[socket]
 	);
 
-	const closeConnection = useCallback(
-		(code, reason) => {
-			socket.close(code, reason);
-		},
-		[socket]
-	);
-
-	useEffect(() => {
-		return closeConnection(1000, "Clean up");
-	}, []);
-
-	return { data, error, close: closeConnection };
+	return { data, error };
 };
